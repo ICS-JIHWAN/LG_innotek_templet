aded4179f86cd89b49fdec3c95bacfb9491199c7
ICS-JIHWAN <103227648+ICS-JIHWAN@users.noreply.github.com>
2024-09-03 15:10:11 +0900

    .mat 파일을 새로운 폴더에 저장 할 수 있게 수정

--- dataset/make_dataset.py
+++ dataset/make_dataset.py
@@ -15,36 +15,43 @@ IMG_FORMATS = ["bmp", "jpg", "jpeg", "png", "tif", "tiff", "dng", "webp", "mpo"]
 def image_to_mat(root_path):
     """
     summary : Data 폴더를 읽은 후 image 파일을 .mat 확장자로 변경 해주는 함수.
-              .mat 확장자 파일에는 정보 (Color image, Gray image, 경로, label, etc..)를 담고 있음.
+              .mat 확장자 파일에는 정보 (Color image, 경로, label, etc..)를 담고 있음.
               Data 폴더 안에 있는 폴더들을 label 로 생각.
+              mat_class 형태의 폴더 안에 새로 저장
     :param root_path: Root path
-    :return:
+    :return: number of classes
     """
     # class 정보
     try:
-        class_names = [name for name in os.listdir(root_path) if os.path.isdir(os.path.join(root_path, name))]
+        class_names = [name for name in os.listdir(root_path) if os.path.isdir(os.path.join(root_path, name)) and not('mat' in name)]
     except:
         raise NotImplementedError('Checking root directory "{}!!"'.format(root_path))
 
     if len(class_names) != 0:
+        num_class = len(class_names)
         # label encoding
         le  = LabelEncoder()
         int_label = le.fit_transform(class_names)
 
         # one hot encoding
-        ohe = OneHotEncoder(sparse=False)
+        ohe = OneHotEncoder(sparse_output=False)
         one_hot_label = ohe.fit_transform(int_label.reshape(len(int_label), 1))
 
         print("데이터 변환 시작...")
         for cls_idx, cls in enumerate(class_names):
+            # original data paths
             data_paths = glob.glob(os.path.join(root_path, cls + '/*'))
+
+            # new directory 생성
+            new_dir = os.path.join(root_path, 'mat_' + cls)
+            os.makedirs(new_dir, exist_ok=True)
 
             pbar = tqdm(enumerate(data_paths))
             for idx, path in pbar:
                 p = Path(path)
 
                 # 변환된 파일이 있으면 제외
-                if (p.parent / f'{p.stem}.mat').exists():
+                if (Path(new_dir) / f'{p.stem}.mat').exists():
                     continue
 
                 try:
@@ -56,15 +63,15 @@ def image_to_mat(root_path):
 
                 # .mat로 저장
                 mat_dict = dict()
-                mat_dict['path']  = path
+                mat_dict['original_path']  = path
                 mat_dict['image'] = im
                 mat_dict['integer_label'] = int_label[cls_idx]
                 mat_dict['one_hot_label'] = one_hot_label[cls_idx]
                 mat_dict['class'] = cls
 
-                savemat(os.path.join(p.parent / f'{p.stem}.mat'), mat_dict)
+                savemat(Path(new_dir) / f'{p.stem}.mat', mat_dict)
 
-        return le, ohe
+        return num_class
     else:
         raise NotImplementedError('There is no class directory in the data folder!!')
 
